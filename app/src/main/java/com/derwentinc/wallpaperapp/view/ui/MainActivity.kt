eb519721915662a281a1ec209706f99891a926fe
Suraj Sharma <sprabhakar@ipass.com>
2018-12-12 15:40:30 +0530

    Moving recyclerview into a fragment

--- app/src/main/java/com/derwentinc/wallpaperapp/view/ui/MainActivity.kt
+++ app/src/main/java/com/derwentinc/wallpaperapp/view/ui/MainActivity.kt
@@ -2,34 +2,12 @@ package com.derwentinc.wallpaperapp.view.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.derwentinc.wallpaperapp.R
-import com.derwentinc.wallpaperapp.view.adapter.PhotoAdapter
-import com.derwentinc.wallpaperapp.viewmodel.PhotoRepositoryImpl
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var photoAdapter: PhotoAdapter
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var photoRepository: PhotoRepositoryImpl
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        photoRepository = PhotoRepositoryImpl("Japan")
-
-        photoAdapter = PhotoAdapter(applicationContext)
-
-        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
-            layoutManager = LinearLayoutManager(applicationContext)
-            itemAnimator = DefaultItemAnimator()
-            adapter = photoAdapter
-        }
-
-        photoRepository.getPhotos().observe(this, Observer { it -> photoAdapter.submitList(it) })
+        setContentView(R.layout.main_activity)
     }
 }
