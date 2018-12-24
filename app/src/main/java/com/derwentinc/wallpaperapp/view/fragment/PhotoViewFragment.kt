efbe8af7c11f602e8aa2691b57959c8034ee13f2
Suraj Sharma <sprabhakar@ipass.com>
2018-12-24 16:01:50 +0530

    Fragment photoview

--- app/src/main/java/com/derwentinc/wallpaperapp/view/fragment/PhotoViewFragment.kt
+++ app/src/main/java/com/derwentinc/wallpaperapp/view/fragment/PhotoViewFragment.kt
@@ -6,33 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.derwentinc.wallpaperapp.R
 import com.derwentinc.wallpaperapp.model.Photo
-import com.derwentinc.wallpaperapp.view.adapter.getScreenHeight
-import com.derwentinc.wallpaperapp.view.adapter.getScreenWidth
 import com.github.chrisbanes.photoview.PhotoView
 
 class PhotoViewFragment : Fragment() {
     private lateinit var imageView: PhotoView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_photo_view, container, false)
-        imageView = view.findViewById(R.id.fragment_photo_view)
-        return view
+        return inflater.inflate(R.layout.fragment_photo_view, container, false).apply { imageView = findViewById(R.id.fragment_photo_view) }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val photo = arguments?.getParcelable<Photo>("photo")
 
-        val screenWidth = getScreenWidth(view.context.applicationContext)
-        val screenHeight = getScreenHeight(view.context.applicationContext)
-        val requestOptions = RequestOptions().fitCenter().override(screenWidth, screenHeight)
         Glide.with(view.context)
-            //.applyDefaultRequestOptions(requestOptions)
             .asBitmap()
-            .load(photo?.url)
+            .load(arguments?.getParcelable<Photo>("photo")?.url)
             .into(imageView)
     }
 
